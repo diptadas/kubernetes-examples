1b429576e6eaa9a16b10c13eb9f540f54e890906
diptadas <dipta@appscode.com>
2018-03-02 11:13:03 +0600

    Refactored codegen

--- hack/build.sh
+++ hack/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -xe
 
-# generate deep-copy
-deepcopy-gen -i ../apis/foocontroller/v1alpha1/
-
 # build binary
 go build -o foo ../main.go
 
@@ -21,14 +18,11 @@ export TLS_SERVING_CERT=$(cat server.crt | ./onessl base64)
 export TLS_SERVING_KEY=$(cat server.key | ./onessl base64)
 export KUBE_CA=$(./onessl get kube-ca | ./onessl base64)
 
-# create CRD
-# cat foo-crd.yaml | ./onessl envsubst | kubectl apply -f -
-
 # create operator deployment, service and tls secret
 cat operator.yaml | ./onessl envsubst | kubectl apply -f -
 
-# create ValidatingWebhookConfiguration
-# cat admission.yaml | ./onessl envsubst | kubectl apply -f -
-
 # create APIService
 cat apiservice.yaml | ./onessl envsubst | kubectl apply -f -
+
+# cleanup
+rm foo ca.crt ca.key server.crt server.key
--- hack/codegen.sh
+++ hack/codegen.sh
@@ -3,7 +3,7 @@
 set -x
 
 GOPATH=$(go env GOPATH)
-PACKAGE_NAME=k8s-admission-webhook
+PACKAGE_NAME=k8s-extension-apiserver
 REPO_ROOT="$GOPATH/src/$PACKAGE_NAME"
 DOCKER_REPO_ROOT="/go/src/$PACKAGE_NAME"
 DOCKER_CODEGEN_PKG="/go/src/k8s.io/code-generator"
@@ -15,10 +15,10 @@ rm -rf "$REPO_ROOT"/apis/foocontroller/v1alpha1/*.generated.go
 docker run --rm -ti -u $(id -u):$(id -g) \
   -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
   -w "$DOCKER_REPO_ROOT" \
-  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-internal-groups.sh all \
-  k8s-admission-webhook/client \
-  k8s-admission-webhook/apis \
-  k8s-admission-webhook/apis \
+  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-internal-groups.sh deepcopy \
+  "$PACKAGE_NAME"/client \
+  "$PACKAGE_NAME"/apis \
+  "$PACKAGE_NAME"/apis \
   foocontroller:v1alpha1 \
   --go-header-file "$DOCKER_REPO_ROOT/hack/boilerplate.go.txt"
 
