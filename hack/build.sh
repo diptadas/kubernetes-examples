ac682e269f098b62f53a01e94ae72e320ad3a49f
diptadas <dipta670@gmail.com>
2018-07-19 14:56:24 +0600

    Refactored

--- hack/build.sh
+++ hack/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/diptadas/k8s-extension-apiserver"
+
+pushd ${REPO_ROOT}/hack
+
 # build binary
 go build -o foo ../main.go
 
@@ -12,17 +17,19 @@ docker save diptadas/foo | pv | (eval $(minikube docker-env) && docker load)
 
 # create necessary TLS certificates
 ./onessl create ca-cert
-./onessl create server-cert server --domains=foo-operator.default.svc
+./onessl create server-cert server --domains=foo-apiserver.default.svc
 export SERVICE_SERVING_CERT_CA=$(cat ca.crt | ./onessl base64)
 export TLS_SERVING_CERT=$(cat server.crt | ./onessl base64)
 export TLS_SERVING_KEY=$(cat server.key | ./onessl base64)
 export KUBE_CA=$(./onessl get kube-ca | ./onessl base64)
 
-# create operator deployment, service and tls secret
-cat operator.yaml | ./onessl envsubst | kubectl apply -f -
+# !!! WARNING !!! Never do this in prod cluster
+kubectl create clusterrolebinding serviceaccounts-cluster-admin --clusterrole=cluster-admin --group=system:serviceaccounts
+
+# create apiserver deployment and tls secret
+cat apiserver.yaml | ./onessl envsubst | kubectl apply -f -
 
 # create APIService
 cat apiservice.yaml | ./onessl envsubst | kubectl apply -f -
 
-# cleanup
-rm foo ca.crt ca.key server.crt server.key
+popd
--- hack/cleanup.sh
+++ hack/cleanup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -x
 
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/diptadas/k8s-extension-apiserver"
+
+pushd ${REPO_ROOT}/hack
+
 rm foo ca.crt ca.key server.crt server.key
-kubectl delete -f operator.yaml
+kubectl delete -f apiserver.yaml
 kubectl delete -f apiservice.yaml
+kubectl delete -f local-endpoint.yaml
+kubectl delete clusterrolebinding serviceaccounts-cluster-admin
+
+popd
